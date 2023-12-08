7f93a74df864cb8910fbb89156ab479b998a80be
Manish Prajapati <manishprajapatim416@gmail.com>
2023-12-09 00:02:49 +0530

    Bouncing a sphere

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@ const gui = new dat.GUI();
 const options = {
   sphereColor: "#ffea00",
   wireframe: false,
+  speed: 0.01,
 };
 
 gui.addColor(options, "sphereColor").onChange(function (e) {
@@ -68,9 +69,17 @@ gui.add(options, "wireframe").onChange((e) => {
   sphere.material.wireframe = e;
 });
 
+gui.add(options, "speed", 0, 0.1);
+
+let step = 0;
+
 function animate(time) {
   box.rotation.x = time / 1000;
   box.rotation.y = time / 1000;
+
+  step += options.speed;
+  sphere.position.y = 10 * Math.abs(Math.sin(step));
+
   renderer.render(scene, camera);
 }
 
