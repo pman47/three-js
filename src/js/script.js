9c53771b86057067349bf3b73e807567887189ba
Manish Prajapati <manishprajapatim416@gmail.com>
2023-12-08 23:33:10 +0530

    Added plane to the scene

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -21,13 +21,18 @@ const orbit = new OrbitControls(camera, renderer.domElement);
 const axisHelper = new THREE.AxesHelper(3);
 scene.add(axisHelper);
 
-camera.position.set(0, 2, 5);
+camera.position.set(-10, 30, 30);
 orbit.update();
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
 
 function animate(time) {
   box.rotation.x = time / 1000;
